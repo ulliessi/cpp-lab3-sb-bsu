005a98bf19061448503e7aabf94c13ce269d6102
ulliessi <151785995+ulliessi@users.noreply.github.com>
2023-11-27 12:43:51 +0300

    3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,14 +1,33 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": ";
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
-return 0;
+    int k, i = 1, sum_k = 0, number;
+    cout << "Enter the amount of integers to sum = ";
+    cin >> k;
+
+    if (k > 0) {
+        do {
+            cout << "Enter integer nr. " << i << ": ";
+            cin >> number;
+
+            if (cin.fail()) {
+                cout << "Invalid input. Please enter an integer." << endl;
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                continue;
+            }
+
+            sum_k += number;
+            i++;
+        } while (i <= k);
+
+        cout << "The total sum of " << k << " integers is: " << sum_k;
+    }
+    else {
+        cout << "Invalid value";
+    }
+
+    return 0;
 }
